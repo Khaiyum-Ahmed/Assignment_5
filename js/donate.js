ac12b6c779928eca3d0ccdf8601082f35906ada8
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-07-01 01:36:03 +0600

    all donation calculation complete

--- js/donate.js
+++ js/donate.js
@@ -14,20 +14,29 @@ document.getElementById('blog-button').addEventListener('click', function(){
 // history button...
 
 document.getElementById('history-button').addEventListener('click', function(){
+    // button design swap
     document.getElementById('history-button').classList.remove('default');
     document.getElementById('history-button').classList.add('active');
     document.getElementById('donation-button').classList.remove('active');
     document.getElementById('donation-button').classList.add('default');
 
+    // button section swap
+    document.getElementById('donation-card').classList.add('hidden');
+    document.getElementById('history-container').classList.remove('hidden');
+
     
 });
 
 document.getElementById('donation-button').addEventListener('click', function(){
+    // button design swap
     document.getElementById('donation-button').classList.remove('default');
     document.getElementById('donation-button').classList.add('active');
     document.getElementById('history-button').classList.remove('active');
     document.getElementById('history-button').classList.add('default');
     
+    // button section swap
+    document.getElementById('donation-card').classList.remove('hidden');
+    document.getElementById('history-container').classList.add('hidden');
     
 });
 
@@ -56,14 +65,12 @@ document.getElementById('noakhali-btn').addEventListener('click', function(){
     if(isNaN(donateAmount)){
         alert('Invalid Donation, please try again');
         document.getElementById('input-donate-noakhali').value = '';
-        document.getElementById('my_modal_1').classList.add('hidden');
         return;
     }
 
     else if(donateAmount > mainBalance){
         alert('Insufficient Balance to Donate');
         document.getElementById('input-donate-noakhali').value = '';
-        document.getElementById('my_modal_1').classList.add('hidden');
         return;
     }
 
@@ -77,20 +84,149 @@ document.getElementById('noakhali-btn').addEventListener('click', function(){
 
     // modal 
 
-    // const modal = my_modal_1.showModal();
-    // return modal;
 
-    document.getElementById('noakhali-btn-close').addEventListener('click', function(){
-        document.getElementById('my_modal_1').classList.add('hidden');
-        return;
-    });
-    document.getElementById('input-donate-noakhali').value = '';
 
-    // history.....
+      // history.....
 
     const div = document.createElement('div');
+    let now = new Date();
+    div.innerHTML = `
+        <div class = "mb-6">
+            <h2 class="text-xl font-semibold text-[#111] mb-4 " > ${donateAmount} Taka is Donated for Flood at Noakhali, Bangladesh</h2>
+            <p class ="text-[rgba(17,17,17,.7)] text-base font-light "> Date ${now} </P>
+        </div>
+    `;
+    
+    document.getElementById('content-container').appendChild(div);
+
+   
+
+
+    // const paragraph = document.createElement('p');
+    // const DonationTime = new date();
+    // paragraph.innerText = ` ${donateAmount} Taka is Donated for Flood at Noakhali, Bangladesh`;
+
+    // document.getElementById('content-container').appendChild(paragraph);
+
+
+
+    document.getElementById('input-donate-noakhali').value = '';
+
+
+});
+
+
+// Feni donation section.........
+
+document.getElementById('feni-button').addEventListener('click', function(){
+
+    const donateAmount = getInputFieldValue('input-donate-feni');
+    // console.log(donateAmount);
+
+    const totalDonation = getBalanceValue('feni-balance');
+    // console.log(totalDonation)
+
+    // main-balance
+    const mainBalance = getMainBalanceValue('main-balance');
+    // console.log(mainBalance)
+
+    if(isNaN(donateAmount)){
+        alert('Invalid Donation, please try again');
+        document.getElementById('input-donate-feni').value = '';
+        return;
+    }
+
+    else if(donateAmount > mainBalance){
+        alert('Insufficient Balance to Donate');
+        document.getElementById('input-donate-feni').value = '';
+        return;
+    }
+
+
+    const totalDonationAmount = totalDonation + donateAmount;
+    const remainingMainBalance = mainBalance - donateAmount;
+
+
+    document.getElementById('main-balance').innerText = remainingMainBalance;
+    document.getElementById('feni-balance').innerText = totalDonationAmount;
+
+    // modal 
+
+
+
+      // history.....
+
+    const div = document.createElement('div');
+    let now = new Date();
+    div.innerHTML = `
+        <div class = "mb-6">
+            <h2 class="text-xl font-semibold text-[#111] mb-4 " > ${donateAmount} Taka is Donated for Flood Relief in Feni, Bangladesh</h2>
+            <p class ="text-[rgba(17,17,17,.7)] text-base font-light "> Date ${now} </P>
+        </div>
+    `;
+    
+    document.getElementById('content-container').appendChild(div);
+
+
+
+    document.getElementById('input-donate-feni').value = '';
+});
 
 
 
 
-})
+// Aid for injured section.........
+
+document.getElementById('aid-injured-button').addEventListener('click', function(){
+
+    const donateAmount = getInputFieldValue('input-donate-aid');
+    // console.log(donateAmount);
+
+    const totalDonation = getBalanceValue('aid-injured-balance');
+    // console.log(totalDonation)
+
+    // main-balance
+    const mainBalance = getMainBalanceValue('main-balance');
+    // console.log(mainBalance)
+
+    if(isNaN(donateAmount)){
+        alert('Invalid Donation, please try again');
+        document.getElementById('input-donate-aid').value = '';
+        return;
+    }
+
+    else if(donateAmount > mainBalance){
+        alert('Insufficient Balance to Donate');
+        document.getElementById('input-donate-aid').value = '';
+        return;
+    }
+
+
+    const totalDonationAmount = totalDonation + donateAmount;
+    const remainingMainBalance = mainBalance - donateAmount;
+
+
+    document.getElementById('main-balance').innerText = remainingMainBalance;
+    document.getElementById('aid-injured-balance').innerText = totalDonationAmount;
+
+    // modal 
+
+
+
+      // history.....
+
+    const div = document.createElement('div');
+    let now = new Date();
+    div.innerHTML = `
+        <div class = "mb-6">
+            <h2 class="text-xl font-semibold text-[#111] mb-4 " > ${donateAmount} Taka is Donated for Aid for Injured in the quota Movement</h2>
+            <p class ="text-[rgba(17,17,17,.7)] text-base font-light "> Date ${now} </P>
+        </div>
+    `;
+    
+    document.getElementById('content-container').appendChild(div);
+
+
+
+    document.getElementById('input-donate-aid').value = '';
+});
