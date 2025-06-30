33b8b87a481fd33408a4d05544a935b53f0d2a6e
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-06-30 17:07:31 +0600

    Noakhali donation complete

--- js/blog.js
+++ js/blog.js
@@ -5,3 +5,4 @@ document.getElementById('home-button').addEventListener('click', function(){
     console.log('Home button clicked');
     window.location.href = '/index.html';
 });
+
--- js/donate.js
+++ js/donate.js
@@ -20,21 +20,46 @@ document.getElementById('noakhali-btn').addEventListener('click', function(){
     // console.log('noakhali donate now');
 
     const donateAmount = getInputFieldValue('input-donate-noakhali');
-    console.log(donateAmount);
+    // console.log(donateAmount);
 
     const totalDonation = getBalanceValue('noakhali-balance');
-    console.log(totalDonation)
+    // console.log(totalDonation)
+
+    // main-balance
+    const mainBalance = getMainBalanceValue('main-balance');
+    // console.log(mainBalance)
 
     if(isNaN(donateAmount)){
         alert('Invalid Donation, please try again');
+        document.getElementById('input-donate-noakhali').value = '';
+        document.getElementById('my_modal_1').classList.add('hidden');
         return;
     }
 
-    const totalDonationAmount = totalDonation + donateAmount;
+    else if(donateAmount > mainBalance){
+        alert('Insufficient Balance to Donate');
+        document.getElementById('input-donate-noakhali').value = '';
+        document.getElementById('my_modal_1').classList.add('hidden');
+        return;
+    }
 
+
+    const totalDonationAmount = totalDonation + donateAmount;
+    const remainingMainBalance = mainBalance - donateAmount;
+
+
+    document.getElementById('main-balance').innerText = remainingMainBalance;
     document.getElementById('noakhali-balance').innerText = totalDonationAmount;
 
+    // modal 
 
+    // const modal = my_modal_1.showModal();
+    // return modal;
 
+    document.getElementById('noakhali-btn-close').addEventListener('click', function(){
+        document.getElementById('my_modal_1').classList.add('hidden');
+        return;
+    });
     document.getElementById('input-donate-noakhali').value = '';
+
 })
